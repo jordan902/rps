ea40f06f965284b6c071b30169ee703c610e313c
Jordan MacInnis <macinnis11@gmail.com>
2022-05-07 16:32:58 -0300

    inserts function to check wins/max wins

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
    imgs.forEach((img) =>
    img.addEventListener(('click'), () => {
      if(img.id){
-       playerRound.(img.id);
+       playRound(img.id);
      }
    }))
  }
@@ -18,14 +18,30 @@
 
 
 
- function playerRound(round) {
+ function playerRound(playerChoice) {
+   let wins = checkWins();
+   if (wins >=5) {
+        return 
+   }      
    const computerSelection = computerChoice();
    const winner = checkWinner(playerSelection, computerSelection);
    winners.push(winner);
 
 }
 
- 
+function countWins() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == "Tie").length;
+  document.querySelector('.playerScore').textContent = `Score: ${playerWins}`;
+  document.querySelector('.computerScore').textContent = `Score: ${computerWins}`;
+  document.querySelector('.ties').textContent = `Score: ${ties}`;
+}
+ function checkWins() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  return Math.max(playerWins, computerWins)
+}
 
 function computerChoice() {
   // update dom com selection
