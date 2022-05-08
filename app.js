b8005500dfe90e19406f509f90b305f94e010516
Jordan MacInnis <macinnis11@gmail.com>
2022-05-08 16:44:43 -0300

    end gme function displays winner

--- app.js
+++ app.js
@@ -24,22 +24,43 @@
         return 
    }      
    const computerSelection = computerChoice();
+
    const winner = checkWinner(playerSelection, computerSelection);
    winners.push(winner);]
    countWins();
    displayRound(playerSelection, computerSelection, winner)
+   if (wins ==5){
+     displayEnd()
+   }
+}
+
+function displayEnd() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+
+  if (playerWins == 5) {
+    document.querySelector(".winner").textContent =
+      "You Won 5 Games, Congrats!";
+  } else {
+    document.querySelector(".winner").textContent =
+      "Sorry, the computer won 5 times";
+  }
+  document.querySelector(".reset").style.display = "flex";
+}
 }
 
 function displayRound(playerSelection, computerSelection)
 document.querySelector('.playerSelection').textContent = `You Chose: $
+
 function countWins() {
-  let playerWins = winners.filter((item) => item == "Player").length;
-  let computerWins = winners.filter((item) => item == "Computer").length;
-  let ties = winners.filter((item) => item == "Tie").length;
-  document.querySelector('.playerScore').textContent = `Score: ${playerWins}`;
-  document.querySelector('.computerScore').textContent = `Score: ${computerWins}`;
-  document.querySelector('.ties').textContent = `Score: ${ties}`;
+  const playerWins = winners.filter((item) => item == "Player").length;
+  const computerWins = winners.filter((item) => item == "Computer").length;
+  const ties = winners.filter((item) => item == "Tie").length;
+  document.querySelector(".playerScore").textContent = `Score: ${playerWins}`;
+  document.querySelector(".computerScore").textContent = `Score: ${computerWins}`;
+  document.querySelector(".ties").textContent = `Score: ${ties}`;
 }
+
+
  function checkWins() {
   let playerWins = winners.filter((item) => item == "Player").length;
   let computerWins = winners.filter((item) => item == "Computer").length;
